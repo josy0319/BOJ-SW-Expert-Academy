01732a2946b7a1ea310bd94a06a8e769a12a6b32
josy0319 <48201370+josy0319@users.noreply.github.com>
2019-12-20 14:48:31 +0900

    Create 탑.py

---
+++ Programmers/탑.py
@@ -0,0 +1,14 @@
+def solution(heights):
+    answer = []
+    #4 7 5 9 6
+    for i in range(len(heights)-1,-1,-1):
+        for j in range(i-1,-1,-1):
+            if heights[i] < heights[j]:
+                answer.append(j+1)
+                break    
+            if j == 0:
+                answer.append(0)
+                break
+    answer.append(0)
+    answer.reverse()
+    return answer
