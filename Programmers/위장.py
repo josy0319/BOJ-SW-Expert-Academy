5159f501deafc674c897f5f6bfeabb857c30cdbb
josy0319 <48201370+josy0319@users.noreply.github.com>
2020-01-19 22:18:49 +0900

    Update 위장.py

--- Programmers/위장.py
+++ Programmers/위장.py
@@ -1,4 +1,6 @@
 #Programmers - 위장
+#두 번째 풀이 -> Counter의 응용으로 코드를 반 이상 줄임
+#Counter는 key, value형태인 dictionary형태로 반환해줌
 
 def solution(clothes):
     answer = 0
@@ -20,4 +22,18 @@ def solution(clothes):
     for i in range(0,len(list)):
         sum = sum * (list[i]+1)
     answer = sum -1
+    return answer
+
+
+#######################################################
+
+
+from collections import Counter
+def solution(clothes):
+    answer = 0
+    sum = 1
+    cnt = Counter([kind for name, kind in clothes])
+    for i in cnt.values() :
+        sum = sum * (i+1)
+    answer = sum - 1
     return answer
