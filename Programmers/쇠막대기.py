7df2ee5ed6d1cfa9d2f3c8cd1ddc7920db71c06f
josy0319 <48201370+josy0319@users.noreply.github.com>
2020-02-18 18:21:06 +0900

    Update 쇠막대기.py

--- Programmers/쇠막대기.py
+++ Programmers/쇠막대기.py
@@ -1,5 +1,6 @@
 #Programmers - 쇠막대기
 
+
 def solution(arrangement):
     answer = 0
     arrangement = arrangement.replace('()',"L")
@@ -13,3 +14,34 @@ def solution(arrangement):
             temp.pop()
             answer += 1
     return answer
+
+
+
+if __name__=="__main__":
+    n = list(input())
+    res = 0
+    #res는 막대기 값
+    i, cnt = 0, 0
+    #i는 순차적으로 검사하는 인덱스
+    #cnt는 누적된 (의 갯수
+    while i < len(n):
+        temp = n[i]
+        #(만 나와서 +=1해야되는 경우
+        if temp == '(':
+            if n[i+1] != ')':
+                cnt += 1
+                res += 1
+            else:
+                cnt +=  1
+        #()가나와서 쏘는 경우
+        elif temp == ')' and n[i-1] == '(':
+            if cnt > 1:
+                cnt -= 1
+                res += cnt
+            else:
+                cnt = 0
+        #)만 나오는 경우
+        elif temp == ')':
+            cnt -= 1
+        i+=1
+    print(res)
