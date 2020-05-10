be258992837065a20f97b716e41f1ce94c414a19
josy0319 <48201370+josy0319@users.noreply.github.com>
2020-05-10 18:56:18 +0900

    Create 크레인_인형뽑기_게임.py

---
+++ Programmers/크레인_인형뽑기_게임.py
@@ -0,0 +1,22 @@
+#Programmers - 크레인_인형뽑기_게임
+
+
+
+
+def solution(board, moves):
+    res = 0
+    s = []
+    board = list(map(list, zip(*board)))
+    for i in moves:
+        for idx, j in enumerate(board[i-1]):
+            if j!=0:
+                if len(s)>0 and s[-1] == board[i-1][idx]:
+                        res+=2
+                        board[i-1][idx]=0
+                        s.pop()
+                        break
+                else:
+                    s.append(board[i-1][idx])
+                    board[i-1][idx]=0
+                    break
+    return res
